787af1c2bb3516d09088566c0a3768652c0c9e58
s1041613 <35199238+s1041613@users.noreply.github.com>
2019-07-19 05:24:33 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -16,19 +16,6 @@ module.exports = {
       port:8081,//端口号
       https:false,//是否开启协议名,如果开启会发出警告
       hotOnly:false,//热模块更新的一种东西,webpack中自动有过配置,但如果我们下载一些新            的模块可以更好的给我更新一些配置
-      // proxy:{
-      // //配置跨域
-      // // '/api':{//配置跨域的名字
-      // //     target:'http//localhost:5000/api',//跨域的地址
-      // //     ws:true,
-      // //     changOrigin:true,//是否跨域
-      // //     pathRewrite:{//当前的名字
-      // //         '^/api':''
-      // //     }
-      // //   }
-
-      // }
-      
   }
 };
 
